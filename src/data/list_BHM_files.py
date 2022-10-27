e8b9d77d7a5a36c88823b928e95120c6984e16d6
Tim Callow <timjcallow@gmail.com>
2022-10-27 14:40:26 +0100

    Make script to extract BHM tif file names

---
+++ src/data/list_BHM_files.py
@@ -0,0 +1,22 @@
+"""
+List all the BHM (non-merged) files from the AWS data folder.
+"""
+
+# file imports
+import glob
+
+# path to data
+datapath = "/home/tim/data/UNICEF_data/height-model/"
+
+# search through BHM folders and add them to list
+folder_search = glob.glob(datapath + "????-BHM")
+folder_list = []
+for folder in folder_search:
+    folder_list.append(folder)
+
+# search through files and write them to file
+with open("BHM_file_list.txt", "w") as f:
+    for folder in folder_list:
+        file_search = glob.glob(folder + "/BHM-????-???.tif")
+        for file in file_search:
+            f.write(file + "\n")
