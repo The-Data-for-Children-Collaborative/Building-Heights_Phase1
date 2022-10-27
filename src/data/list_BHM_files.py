deaa7091bc97b9f280a91810b75adaff801550a7
Tim Callow <timjcallow@gmail.com>
2022-10-27 15:45:13 +0100

    New writepath

--- src/data/list_BHM_files.py
+++ src/data/list_BHM_files.py
@@ -4,9 +4,17 @@ List all the BHM (non-merged) files from the AWS data folder.
 
 # file imports
 import glob
+import os
+
+# get the current working directory
+cwd = os.getcwd()
 
 # path to data: must change depending on username!
 datapath = "/home/tim/data/UNICEF_data/height-model/"
+
+# path to write file to (no need to change)
+writepath = "/".join(cwd.split("/")[:4]) + "/data/processed/"
+
 
 # search through BHM folders and add them to list
 folder_search = glob.glob(datapath + "????-BHM")
@@ -15,7 +23,7 @@ for folder in folder_search:
     folder_list.append(folder)
 
 # search through files and write them to file
-with open(datapath + "BHM_file_list.txt", "w") as f:
+with open(writepath + "BHM_file_list.txt", "w") as f:
     for folder in folder_list:
         file_search = glob.glob(folder + "/BHM-????-???.tif")
         for file in file_search:
