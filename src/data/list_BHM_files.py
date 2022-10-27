1610685a05bf2458685e0849ce2c77af220d207f
Tim Callow <timjcallow@gmail.com>
2022-10-27 15:24:41 +0100

    Write filenames to data folder instead

--- src/data/list_BHM_files.py
+++ src/data/list_BHM_files.py
@@ -5,7 +5,7 @@ List all the BHM (non-merged) files from the AWS data folder.
 # file imports
 import glob
 
-# path to data
+# path to data: must change depending on username!
 datapath = "/home/tim/data/UNICEF_data/height-model/"
 
 # search through BHM folders and add them to list
@@ -15,7 +15,7 @@ for folder in folder_search:
     folder_list.append(folder)
 
 # search through files and write them to file
-with open("BHM_file_list.txt", "w") as f:
+with open(datapath + "BHM_file_list.txt", "w") as f:
     for folder in folder_list:
         file_search = glob.glob(folder + "/BHM-????-???.tif")
         for file in file_search:
