99efb5c03ab4f26fedaf3f202971a9d61212958c
Tim Callow <timjcallow@gmail.com>
2022-10-30 19:21:45 +0000

    Switch to EPSG-3857 co-ordinates to match maxar data

--- src/data/download_sentinel_2_data.py
+++ src/data/download_sentinel_2_data.py
@@ -4,12 +4,9 @@ A script to download the relevant data from the Sentinel 2 sattelite.
 
 # package imports
 import ee
-import matplotlib.pyplot as plt
 import numpy as np
-import IPython.display as disp
 import pandas as pd
-import utm
-import rioxarray
+from pyproj import Transformer
 import urllib.request
 
 
@@ -22,7 +19,7 @@ image_bounds = [0, 3000]
 
 
 # authenticate and initialize gee
-# ee.Authenticate()
+ee.Authenticate()
 ee.Initialize()
 
 
@@ -34,14 +31,13 @@ def load_ref_coords(path, filename):
 
 
 def save_sentinel_image(
-    filename, image_collection, date_initial, date_final, image_bounds, bands
+    df, i, image_collection, date_initial, date_final, image_bounds, bands
 ):
 
     # first retrieve the co-ordinates
-    coords = get_coords_from_tif(filename)
+    coords, filecode = get_latlon_coords(df, i)
 
     # strip the relevant info from filename
-    filecode = filename[-12:-4]
     sentinel_filename = "stl2_" + filecode + "_.png"
 
     # get the fdb image
@@ -59,7 +55,6 @@ def save_sentinel_image(
 def get_fdb_image(image_collection, coords, date_initial, date_final):
     """Get an fdb image from a set of co-ordinates."""
     aoi = ee.Geometry.Polygon(coords)
-    print(aoi)
     ffa_db = ee.Image(
         ee.ImageCollection(image_collection)
         .filterBounds(aoi)
@@ -71,46 +66,40 @@ def get_fdb_image(image_collection, coords, date_initial, date_final):
     return ffa_db
 
 
-def get_coords_from_tif(filename):
+def get_latlon_coords(df_coords, i):
     """Returns a set of lat/long co-ordinates from tif file."""
 
-    # load in the array
-    array = rioxarray.open_rasterio(filename, masked=True).squeeze()
+    # choose a given row
+    coord_row = df_coords.iloc[[i]]
 
-    # get the bounding co-ordinates
-    x0 = float(array.x[0])
-    y0 = float(array.y[0])
-    x1 = float(array.x[-1])
-    y1 = float(array.y[-1])
+    # retrive the co-ordinates
+    x0 = coord_row.left.values[i]
+    x1 = coord_row.right.values[i]
+    y0 = coord_row.bottom.values[i]
+    y1 = coord_row.top.values[i]
+    coords_3857 = np.array([[x0, y0], [x0, y1], [x1, y1], [x1, y0]])
 
-    # set up the corners of the image square
-    bl = utm.to_latlon(x0, y0, 23, "K")
-    br = utm.to_latlon(x1, y0, 23, "K")
-    tr = utm.to_latlon(x1, y1, 23, "K")
-    tl = utm.to_latlon(x0, y1, 23, "K")
+    # change - to _ in filename to match with maxar data
+    filename = "_".join(coord_row.qmdt_cod.values[0].split("-"))
 
-    print(bl, br, tr, tl)
+    # define the transformer
+    transformer = Transformer.from_crs("EPSG:3857", "EPSG:4326", always_xy=True)
 
-    # make a list of the lat-long pairs
-    coord_list = []
-    for coord in [bl, br, tr, tl]:
-        coord_list.append(list(coord)[::-1])
-    return coord_list
+    # transform to lat lon (epsg:4326)
+    coords_x, coords_y = transformer.transform(coords_3857[:, 0], coords_3857[:, 1])
+    coords_latlon = np.dstack([coords_x, coords_y])[0].tolist()
+
+    return coords_latlon, filename
 
 
 if __name__ == "__main__":
-    # df_coords = load_ref_coords(
-    #     "/home/callow46/Autumn22_DFCCU/data/processed/", "grids_extent.csv"
-    # )
-    # print(df_coords.head())
-    # print(df_coords.info())
+    df_coords = load_ref_coords("/home/tim/data/UNICEF_data/", "grids_extent.csv")
+    print(df_coords.head())
+    print(df_coords.info())
 
-    coord_0 = get_coords_from_tif(
-        "/home/callow46/Autumn22_DFCCU/data/raw/test_data/BHM-2222-224.tif"
-    )
-
-    filename = "/home/callow46/Autumn22_DFCCU/data/raw/test_data/BHM-2222-224.tif"
+    coords_latlon, filename = get_latlon_coords(df_coords, 0)
+    print(coords_latlon, filename)
 
     save_sentinel_image(
-        filename, image_collection, start_date, end_date, image_bounds, bands
+        df_coords, 0, image_collection, start_date, end_date, image_bounds, bands
     )
