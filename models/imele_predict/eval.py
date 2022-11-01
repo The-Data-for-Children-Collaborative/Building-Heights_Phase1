cc2c41de42055af8b19fa431adc22a38f7a6514f
Giacomo Bighin <bighin@gmail.com>
2022-11-01 12:07:14 +0100

    Added docstrings and comments.

--- models/imele_predict/eval.py
+++ models/imele_predict/eval.py
@@ -3,7 +3,6 @@ import torch.nn.parallel
 from torch.utils.data import Dataset, DataLoader
 
 import torchvision
-#from torchvision import transforms, utils
 
 import pandas as pd
 import numpy as np
@@ -13,14 +12,15 @@ from PIL import Image
 from models import modules, net, resnet, densenet, senet
 from transforms import ToTensor, Normalize
 
-
 class depthDataset(Dataset):
+    '''
+        Our super-simple data loader, an implementation of an torch.utils.data.DataLoader object
+    '''
 
     def __init__(self, csv_file, transform=None):
 
         self.frame = pd.read_csv(csv_file, header=None)
         self.transform = transform
-
 
     def __getitem__(self, idx):
 
@@ -34,15 +34,30 @@ class depthDataset(Dataset):
 
         return sample
 
-
     def __len__(self):
         return len(self.frame)
 
 
-def get_evaluation_data(batch_size=16, csv_file=''):
+def get_evaluation_data(batch_size=1, csv_file=''):
+    '''
+        Loads the evaluation data from a CSV file
+
+        Input parameters: batch_size, the number of samples in a batch
+                          csv_file, a file containing the input data
+
+        Return type: a torch.utils.data.DataLoader object
+    '''
 
     imagenet_stats = {'mean': [0.485, 0.456, 0.406],
                       'std': [0.229, 0.224, 0.225]}
+
+    # A transform is applied to the input data, converting it
+    # to tensor format and normalizing it to have the same mean
+    # and standard deviation of the ImageNet dataset
+    #
+    # This is related to the fact the SENet 'backbone' is indeed
+    # trained on the ImageNet dataset, but I am not sure I fully
+    # understand this.
 
     my_transform = torchvision.transforms.Compose([
                                            ToTensor(),
@@ -53,6 +68,9 @@ def get_evaluation_data(batch_size=16, csv_file=''):
     transformed_testing = depthDataset(csv_file=csv_file,
                                        transform=my_transform)
 
+    # One should check carefully num_workers and pin_memory,
+    # which could be important once we use CUDA.
+
     dataloader_testing = DataLoader(transformed_testing, batch_size,
                                     shuffle=False, num_workers=0, pin_memory=False)
 
@@ -60,6 +78,15 @@ def get_evaluation_data(batch_size=16, csv_file=''):
 
 
 def define_model(is_resnet, is_densenet, is_senet):
+    '''
+        Selects a model to use.
+
+        A pretrained CNN (ResNet, DenseNet or SENet) is
+        used for transfer learning, and is 'encoded' to form our final model.
+
+        Note: we only worked with SENet so far, other models are completely
+        untested.
+    '''
 
     if is_resnet:
         original_model = resnet.resnet50(pretrained = True)
@@ -78,11 +105,22 @@ def define_model(is_resnet, is_densenet, is_senet):
 
     return model
 
-
 def eval_main():
+    '''
+        The main function.
+
+        Loads the dataset, the pre-trained networks and
+        evaluates it over the input data.
+    '''
 
     csv_filename='test0.csv'
     model_filename='../../data/external/Block0_skip_model_110.pth.tar'
+
+    # Input images can be load in batches, resulting in a tensor of shape
+    # [ nr_batches, nr_channels, x_dim, y_dim ]
+    #
+    # Batches do not make a big difference when running on CPU, but they
+    # speed up the process a lot when running on GPU/CUDA.
 
     batch_size = 1
     test_loader = get_evaluation_data(batch_size, csv_filename)
@@ -90,15 +128,18 @@ def eval_main():
     for i, sample_batched in enumerate(test_loader):
 
         image = sample_batched['image']
-        print('Loader i={}: input shape {}'.format(i,image.shape))
+        print('Loaded i = {}, input shape = {}'.format(i,image.shape))
 
+    # Now we can defined the model to be used for evaluation
 
     model = define_model(is_resnet=False, is_densenet=False, is_senet=True)
     model = torch.nn.DataParallel(model,device_ids=[0,1])
+
+    # The pre-trained weights of the model are loaded in a dictionary
+
     state_dict = torch.load(model_filename,map_location=torch.device('cpu'))['state_dict']
 
     # The pre-trained weights are in slightly different format, so that we need to convert their names.
-
 
     new_state_dict = state_dict.copy()
 
@@ -123,15 +164,16 @@ def eval_main():
 
         print('Input #{}, shape is {}'.format(i,image.shape))
 
+        # The model is evaluated on the input data, and the output heightmap
+        # is upsampled from 250x250 to 500x500, to match the input data
+
         output = model(image)
         output = torch.nn.functional.interpolate(output, size=(500,500), mode='bilinear')
 
         print('Output #{}, shape (after resampling) is {}'.format(i,output.shape))
 
-        img1 = output[0]
-        #save_image(img1, 'img{}.png'.format(i))
+        # The output is saved in numpy format
 
-        np.save('img{}.in.npy'.format(i), image.detach().numpy()[0,0], allow_pickle=False)
         np.save('img{}.out.npy'.format(i), output.detach().numpy()[0,0], allow_pickle=False)
 
 
