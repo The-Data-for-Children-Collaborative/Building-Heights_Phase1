421fa0dfa11dfef876a9fd181ec92d41bda0b4e9
Callow <callow46@hemera5.cluster>
2022-11-14 12:50:40 +0100

    Add extra 'F' mode for Brazil BHM data

--- src/models/imele_train/transforms.py
+++ src/models/imele_train/transforms.py
@@ -25,12 +25,13 @@ class ToTensor(object):
 
     def to_tensor(self, pic):
 
-    
         # handle PIL Image
         if pic.mode == 'I':
             img = torch.from_numpy(np.array(pic, np.int32, copy=False))
         elif pic.mode == 'I;16':
             img = torch.from_numpy(np.array(pic, np.int16, copy=False))
+        elif pic.mode == 'F':
+            img = torch.from_numpy(np.array(pic, np.int32, copy=False))            
         else:
             img = torch.ByteTensor(
                 torch.ByteStorage.from_buffer(pic.tobytes()))
@@ -43,6 +44,7 @@ class ToTensor(object):
             nchannel = len(pic.mode)
 
         img = img.view(pic.size[1], pic.size[0], nchannel)
+
         # put it from HWC to CHW format
         # yikes, this transpose takes 80% of the loading time/CPU
         img = img.transpose(0, 1).transpose(0, 2).contiguous()
