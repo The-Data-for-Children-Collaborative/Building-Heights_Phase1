39c1e05836630d6f40377077a670788d4f7d38c0
Ubuntu <ubuntu@ip-172-31-14-252.eu-west-1.compute.internal>
2022-11-19 10:39:34 +0000

    modify script to make predict csv and predict output dir, add script to run predict for model and epoch

--- src/models/imele_predict/make_csv_maxar_trainig_validation_data.py
+++ src/models/imele_predict/make_csv_maxar_trainig_validation_data.py
@@ -13,17 +13,21 @@ short_path="~/data/UNICEF_data/tim_maxar_bhm_final_pairs/pairs_17/"
 path=os.path.expanduser(short_path)
 
 # epoch for which want to make predictions !TODO: call for epochs when running script?
-epochs=[12, 20, 30, 50, 70, 99]
+epochs=[6,20,40]
+
+# which model, model trained on pairs 17 = 17only, treined on pairs 16&17 = 16a17
+model="pair16_17" #TODO call for model name when running script
 
 for epoch in epochs:
     epoch=str(epoch)
 
     #path for predictions !TODO: call for dir name when running script
-    if os.path.isdir(path+"train_maxar_sliced_prediction_17only_epoch"+epoch)==False:
-        command="mkdir "+path+"train_maxar_sliced_prediction_17only_epoch"+epoch
+    prediction_dir=path+"train_maxar_sliced_prediction_"+model+"_epoch"+epoch
+    if os.path.isdir(prediction_dir)==False:
+        command="mkdir "+prediction_dir
         os.system(command)
     else:
-        print("WARNING! \n"+path+"train_maxar_sliced_prediction_17only_epoch"+epoch+" already exists.\n Check content before making predictions saved to this directory!")
+        print("WARNING! \n"+prediction_dir+" already exists.\n Check content before making predictions saved to this directory!")
 
 #load input data and ground truth that are for validation, not trained on 
 #!TODO call for file when running script, or adapt script to run predictions on all input files in a dir
@@ -37,18 +41,19 @@ df_predict_data["prediction"]=df_predict_data.data
 for index, row in df_predict_data.iterrows():
     row.data=path+"train_maxar_sliced/"+os.path.basename(os.path.realpath(row.data))
     row.ground_truth=path+"train_maxar_sliced/"+os.path.basename(os.path.realpath(row.ground_truth))
-    row.prediction=path+"train_maxar_sliced_prediction_17only_epoch"+epoch+"/"+os.path.basename(os.path.realpath(row.data))
 
 for epoch in epochs:
     epoch=str(epoch)
+    prediction_dir=path+"train_maxar_sliced_prediction_"+model+"_epoch"+epoch
     for index, row in df_predict_data.iterrows():
-        row.prediction=path+"train_maxar_sliced_prediction_17only_epoch"+epoch+"/"+os.path.basename(os.path.realpath(row.data))
+        row.prediction=prediction_dir+"/"+os.path.basename(os.path.realpath(row.data))
 
 #save csv file used for eval.py for predictiona
-    prediction_csv="../../../data/processed/prediction_datasets/prediction_maxar_train1711_pair17_epoch"
+    prediction_csv="../../../data/processed/prediction_datasets/prediction_maxar_train1711_"+model+"_epoch"+epoch+".csv"
     if os.path.isfile(prediction_csv)==False:
-        df_predict_data[["data","prediction"]].to_csv(prediction_csv+epoch+".csv",
+        df_predict_data[["data","prediction"]].to_csv(prediction_csv,
                                                 index=False,
                                                 header=False)
+        print(prediction_csv+" created")
     else:
-        print("Warning!"+prediction_csv+"already exists! \n Check"+path+"train_maxar_sliced_prediction_17only_epoch"+epoch+"for already excisting predictions!")
+        print("Warning!"+prediction_csv+"already exists! \n Check "+prediction_dir+"for already excisting predictions!")
