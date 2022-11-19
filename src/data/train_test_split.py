64bb0294b2020871042a636acd41414f94dfa6d0
Tim Callow <timjcallow@gmail.com>
2022-11-19 11:08:54 +0000

    fixes for vhm

--- src/data/train_test_split.py
+++ src/data/train_test_split.py
@@ -27,52 +27,90 @@ def make_split_csvs(folder, filename, train_frac=0.85):
             f.write(filename)
 
 
+def add_vhms_to_csvs(folder):
+
+    splits = ["train", "test"]
+
+    for split in splits:
+
+        with open(folder + "pairings_" + split + ".csv", "r") as f:
+            filenames = f.readlines()
+
+        with open(folder + "triplets_" + split + ".csv", "w") as f:
+            for filename in filenames:
+                maxar, bhm = filename.split(",")
+                vhm = bhm.replace("bhm", "vhm")
+                newline = ",".join([maxar, bhm.strip(), vhm])
+                f.write(newline)
+
+
 def make_train_test_dirs(folder, train_csv, test_csv, zip=False):
 
     # make directories if they don't exist
     dir_bhm_train = folder + "train_bhm_sliced/"
     dir_bhm_test = folder + "test_bhm_sliced/"
+    dir_vhm_train = folder + "train_vhm_sliced/"
+    dir_vhm_test = folder + "test_vhm_sliced/"
     dir_maxar_train = folder + "train_maxar_sliced/"
     dir_maxar_test = folder + "test_maxar_sliced/"
 
-    try:
-        os.mkdir(dir_bhm_test)
-        os.mkdir(dir_bhm_train)
-        os.mkdir(dir_maxar_test)
-        os.mkdir(dir_maxar_train)
-    except FileExistsError:
-        pass
+    for dir in [
+        dir_bhm_train,
+        dir_bhm_test,
+        dir_vhm_train,
+        dir_vhm_test,
+        dir_maxar_train,
+        dir_maxar_test,
+    ]:
+        try:
+            os.mkdir(dir)
+        except FileExistsError:
+            pass
 
-    copy_files(folder, train_csv, dir_maxar_train, dir_bhm_train)
-    copy_files(folder, test_csv, dir_maxar_test, dir_bhm_test)
+    copy_files(folder, train_csv, dir_maxar_train, dir_bhm_train, dir_vhm_train)
+    copy_files(folder, test_csv, dir_maxar_test, dir_bhm_test, dir_vhm_train)
 
 
-def copy_files(input_dir, input_filename, maxar_output_dir, bhm_output_dir):
+def copy_files(
+    input_dir, input_filename, maxar_output_dir, bhm_output_dir, vhm_output_dir
+):
 
     with open(input_dir + input_filename) as f:
         lines = f.readlines()
 
     for i, filenames in enumerate(lines):
-        maxar_filename, bhm_filename = filenames[:-1].split(",")
+        maxar_filename, bhm_filename, vhm_filename = filenames[:-1].split(",")
         print(i, "/", len(lines))
         maxar_array = np.load(input_dir + "sliced_maxar/" + maxar_filename)
         bhm_array = np.load(input_dir + "sliced_bhm/" + bhm_filename)
+        try:
+            vhm_array = np.load(input_dir + "sliced_vhm/" + vhm_filename)
+            print("vhm file", vhm_filename, "not found: making array of zeros")
+        except FileNotFoundError:
+            vhm_array = np.zeros_like(bhm_array)
         if np.shape(maxar_array) == (500, 500, 4):
-            shutil.copyfile(
-                input_dir + "sliced_maxar/" + maxar_filename,
-                maxar_output_dir + maxar_filename,
-            )
+            np.save(maxar_output_dir + maxar_filename, maxar_array)
+            # shutil.copyfile(
+            #     input_dir + "sliced_maxar/" + maxar_filename,
+            #     maxar_output_dir + maxar_filename,
+            # )
         if np.shape(bhm_array) == (500, 500):
-            shutil.copyfile(
-                input_dir + "sliced_bhm/" + bhm_filename, bhm_output_dir + bhm_filename
-            )
+            np.save(bhm_output_dir + bhm_filename, bhm_array)
+            # shutil.copyfile(
+            #     input_dir + "sliced_bhm/" + bhm_filename, bhm_output_dir + bhm_filename
+            # )
+
+        if np.shape(vhm_array) == (500, 500):
+            np.save(vhm_output_dir + vhm_filename, vhm_array)
 
 
 if __name__ == "__main__":
 
-    folder = "/home/tim/data/UNICEF_data/tim_maxar_bhm_final_pairs/pairs_16/"
+    folder = "/home/tim/data/UNICEF_data/tim_maxar_bhm_final_pairs/pairs_10/"
 
-    if not os.path.isfile(folder + "pairings_train.csv"):
+    force_overwrite = True
+    if not os.path.isfile(folder + "pairings_train.csv") or force_overwrite:
+        print("making csvs")
         make_split_csvs(folder, "pairings.csv")
-
-    make_train_test_dirs(folder, "pairings_train.csv", "pairings_test.csv")
+        add_vhms_to_csvs(folder)
+    make_train_test_dirs(folder, "triplets_train.csv", "triplets_test.csv")
