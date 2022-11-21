8599d20038ccb9cae7b934e32f983549f5bf6c47
Ubuntu <ubuntu@ip-172-31-14-252.eu-west-1.compute.internal>
2022-11-21 19:00:20 +0000

    add routines to create csv file and run predictions

--- src/models/imele_predict/run_predict.py
+++ src/models/imele_predict/run_predict.py
@@ -6,22 +6,26 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-e', '--epochs')
 parser.add_argument('-md', '--modeldir')
 parser.add_argument('-m', '--model')
+parser.add_argument('-p', '--pairs')
 args = parser.parse_args()
 
-#path for data
-short_path="~/data/UNICEF_data/tim_maxar_bhm_final_pairs/pairs_17/"
-path=os.path.expanduser(short_path)
-
-# epoch for which want to make predictions !TODO: call for epochs when running script?
+# epoch for which want to make predictions 
 epochs=(args.epochs)
 epochs=epochs.split(',')
 
+
+# pairs for which want to make predictions, e.g. pairs_17 
+pairs=(args.pairs)
+pairs=pairs.split(',')
+
 # which model, model trained on pairs 17 = 17only, treined on pairs 16&17 = 16a17
-model=str(args.model) #TODO call for model name when running script
+model=str(args.model) 
 model_dir=os.path.expanduser(args.modeldir)
 
-for epoch in epochs:
-        epoch=str(epoch)
-        prediction_csv="../../../data/processed/prediction_datasets/prediction_maxar_train1711_"+model+"_epoch"+epoch+".csv"
-        command="python -u eval.py --csv "+prediction_csv+" --model "+model_dir+"model_"+epoch+".pth.tar | tee log_predict_train1711_"+model+"_epoch"+epoch+".txt"
-        os.system(command)
+for pair in pairs:
+    pair=str(pair)
+    for epoch in epochs:
+            epoch=str(epoch)
+            prediction_csv="../../../data/processed/prediction_datasets/prediction_maxar_"+pair+"_model_"+model+"_epoch"+epoch+".csv"
+            command="python -u eval.py --csv "+prediction_csv+" --model "+model_dir+"model_"+epoch+".pth.tar | tee log_predict_"+model+"_epoch"+epoch+".txt"
+            os.system(command)
