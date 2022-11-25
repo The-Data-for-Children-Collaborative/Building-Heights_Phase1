245e31b8e05c96c97b4e60704c23ae27c5a288cf
InesDana <109284207+InesDana@users.noreply.github.com>
2022-11-25 12:29:03 +0100

    Autoerror (#42)
    * add comments
    
    * add coments and modified check for csv file
    
    Co-authored-by: Ubuntu <ubuntu@ip-172-31-14-252.eu-west-1.compute.internal>

--- src/data/autoerror/autoerror_centroids.py
+++ src/data/autoerror/autoerror_centroids.py
@@ -175,10 +175,10 @@ bhm_directory = 'sliced_bhm'
 
 
 # The directories with the prediction files.
-# Make sure that the directories are somehow 'compatible' (e.g. different pairs id, but same number of epochs),
-# as it wouldn't make much sense to calculate an average error over different epochs.
+# saves a histogram file for each directory, but one csv-file with one row for each prediction directory
+# prediction files must start with maxar- in this set up. And ground_truth data must start with bhm-
 
-prediction_directories = ['train_maxar_sliced_prediction_17only_epoch3',
+prediction_directories = ['train_maxar_sliced_prediction_17only_epoch3'],
                             'train_maxar_sliced_prediction_17only_epoch20',
                             'train_maxar_sliced_prediction_17only_epoch50',
                             'train_maxar_sliced_prediction_pair16_17_epoch50']
@@ -236,10 +236,18 @@ for index,row in df.iterrows():
     row.all_error=list(map(lambda x: x['error'], histogram))
     row.all_heights=list(map(lambda x: x['height'], histogram))
 
-error_csv=path+"prediction_error/"+"centroid_error.csv"
-if os.path.isfile(error_csv)==False:
-        df[["prediction_dir","abs_error","all_error"]].to_csv(error_csv)
-        print(error_csv+" created")
+
+
+error_csv=path+"centroid_error.csv"
+if os.path.isfile(error_csv)==True:
+        print("WARNING! \n"+error_csv+" already exists.\n")
+        abort= input("Do you want to continue?  [y/n] ")
+        if abort=="n":
+            exit()
+df[["prediction_dir","abs_error","all_error"]].to_csv(error_csv)
+print(error_csv+" created")
+
+
 # Here one could plot the histograms
 # Something like
 # import seaborn as sns
@@ -247,3 +255,38 @@ if os.path.isfile(error_csv)==False:
 # or also
 # sns.violinplot(data=histogram, palette="muted", split=True, cut=0)
 # and then saving it to a file? What's the command?
+
+
+# Now we do the same, but with error_on_segments()
+# running over miltiple directories for different epochs has not been extended to segment error calculation. Therefore the code below is commented.
+""" absolute_error = 0
+counter = 0
+histogram = []
+
+for pair in pairs:
+
+    if not os.path.exists(pair['bhm']):
+        continue
+
+    if not os.path.exists(pair['prediction']):
+        continue
+
+    print('Loading {} (BHM) and {} (prediction)'.format(pair['bhm'], pair['prediction']))
+
+    error, mean, all_errors = error_on_segments(pair['bhm'], pair['prediction'])
+
+    absolute_error += error * mean
+    counter += 1
+    histogram.extend(all_errors)
+
+absolute_error /= counter
+
+print('Absolute error (in meters) calculated over {} files, segment-by-segment: {}'.format(counter, absolute_error))
+
+with open(str(df.prediction_dir[0])+"segments.histogram", "wb") as fp:
+    pickle.dump(histogram, fp)
+ """
+# Also here, one could save the histogram!
+
+# Finally, one can define maybe 3 or 4 classes of buildings, by height. And evaluate the errors (also with histograms), class by class.
+
